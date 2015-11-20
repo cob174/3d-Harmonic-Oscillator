fb9121d82fc65b0212175667e53a1264b23b131e
cob174 <michaelch174@gmail.com>
2015-11-20 02:32:54 -0500

    working version

--- 3dharmonisoscillator.py
+++ 3dharmonisoscillator.py
@@ -3,13 +3,35 @@
 #michael chiong
 #emilee conard
 
-z = [0, h]
-u = [0, h**(l+1)]
-zmax = 0
-h = .01
-n = 1
+import math
+import matplotlib.pyplot as pyplot
 
-while (n < 4)
-	while (z < zmax)
+n=4.
+epsilon=n+1.5
+h=0.01
+u0=0
+u1=0.01
+z=0.01
+zmax=8.
+l=0
 
-	n + 1
+zList=[0,h]
+uList=[0,h**(l+1)]
+
+while True:
+  u2=(2+(h**2.)*((0./(z**2.))+(0.25*(z**2.))-epsilon))*u1-u0
+  z=z+h
+
+  u0=u1
+  u1=u2
+
+  u2=u2**2.
+
+  zList.append(z)
+  uList.append(u2)
+
+  if zmax<z:
+    break;
+
+pyplot.plot(zList, uList)
+pyplot.show()
